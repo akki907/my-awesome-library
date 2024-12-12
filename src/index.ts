07f1a9e8edbeb459d6d7e03f9ae95a14dce97a33
Akash kumar <ak.akki907@gmail.com>
2024-12-12 18:49:11 +0530

    husky integration check

--- src/index.ts
+++ src/index.ts
@@ -140,8 +140,12 @@ export class AwesomeLibrary {
      * @param date2 - Second date
      * @returns Number of days between dates
      */
+
+
     static daysBetween(date1: Date, date2: Date): number {
         const oneDay = 24 * 60 * 60 * 1000;
+
+
         return Math.round(Math.abs((date1.getTime() - date2.getTime()) / oneDay));
     }
 }
